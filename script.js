740a0ef81ba9a828c1421ed20da325f8c7216284
flack <163582560+FlackCode@users.noreply.github.com>
2024-03-23 11:57:40 +0200

    Working Addition!
    Addition in the calc finally works but now i have to figure out a better way of writing all the code

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let operator = ``;
-let num1 = ``;
-let num2 = ``;
+let num1 = null;
+let num2 = null;
 const display = document.getElementById(`display`)
 let displayContent = display.textContent;
 display.textContent = `0`;
+//Display Handling !!!
 function Display(){
   display.textContent = displayContent;
 }
@@ -17,13 +18,37 @@ function updateDisplay(buttonValue){
   }
   if(buttonValue == `C`){
     displayContent = ``;
+    num1 = null;
+    num2 = null;
   }
   if (displayContent == '') {
     displayContent = '0';
   }
+  if(buttonValue == `+` || buttonValue == `-` 
+  || buttonValue == `*` || buttonValue == `/`){
+    displayContent = `0`;
+  }
+  if (displayContent.endsWith("=")) {
+    displayContent = displayContent.slice(0, -1);
+  }
   Display();
 }
+//Click Handling !!!
 function buttonClicked(buttonValue){
+  if (buttonValue == `+`) {
+    num1 = Number(displayContent);
+    console.log(num1);
+    displayContent = "0";
+    display.textContent = displayContent;
+  }
+  else if (buttonValue == "=") {
+    num2 = Number(displayContent);
+    console.log(num2);
+    result = num1 + num2;
+    displayContent = result.toString();
+    console.log(displayContent);
+  }
   updateDisplay(buttonValue);
+  Display();
 }
-
+//Math Handling (coming soon in a more optimized version)
