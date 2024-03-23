1b2ad0a95b0f9b2c46c0a5f1b55e03f6ef5abe8e
flack <163582560+FlackCode@users.noreply.github.com>
2024-03-23 14:24:49 +0200

    Handled more calc errors
    Fixed weird behaviors of sqr and sqrt, 1 issue still remains, when pressing a number button after a sqrt result it sets result as num1 instead of resetting everything 
    also need to add abs

--- script.js
+++ script.js
@@ -35,6 +35,12 @@ function updateDisplay(buttonValue){
   if (displayContent.endsWith("=")) {
     displayContent = displayContent.slice(0, -1);
   }
+  if (displayContent.endsWith("square")) {
+    displayContent = displayContent.slice(0, -6);
+  }
+  if (displayContent.endsWith("sqrt")) {
+    displayContent = displayContent.slice(0, -4);
+  }
   if (result == 0){
     displayContent = `0`;
     result = null;
@@ -42,6 +48,18 @@ function updateDisplay(buttonValue){
   if (result === "Division by 0 impossible!" && !isNaN(Number(buttonValue))) {
     displayContent = buttonValue;
     result = null;
+    num1 = null;
+    num2 = null;
+    operator = null;
+  }
+  if (result != null && !isNaN(Number(buttonValue))){
+    displayContent = buttonValue;
+    result = null;
+  }
+  if (result === "Infinity" && !isNaN(Number(buttonValue))) {
+    displayContent = buttonValue;
+    result = null;
+    num1 = null;
   }
   if (buttonValue == `.`&& displayContent.includes(`.`)){
     const decimalBtn = document.getElementById(`decimalBtn`);
@@ -53,19 +71,53 @@ function updateDisplay(buttonValue){
   Display();
 }
 //Click Handling !!!
+function clearAll(){
+  result = null;
+  num1 = null;
+  num2 = null;
+  operator = null;
+}
 function buttonClicked(buttonValue){
   if (buttonValue == `+` || buttonValue == `-` 
   || buttonValue == `*` || buttonValue == `/`) {
-    num1 = Number(displayContent);
-    operator = buttonValue;
-    display.textContent = displayContent;
+    if(num1 == null){
+      num1 = Number(displayContent);
+      operator = buttonValue;
+      display.textContent = displayContent;
+    }
+    else{
+      num2 = Number(displayContent);
+      console.log(num1, operator, num2);
+      doMath(num1, operator, num2);
+      console.log(result);
+      num1 = Number(result);
+      operator = buttonValue;
+      displayContent = result;
+    }
   }
-  if (buttonValue == "=") {
+  else if (buttonValue == "=" && num1 != null && operator != null) {
     num2 = Number(displayContent);
     console.log(num1, operator, num2);
     doMath(num1, operator, num2);
     console.log(result);
     displayContent = result;
+    num1 = Number(result);
+    console.log(typeof(result));
+    operator = null;
+    num2 = null;
+  }
+  else if (buttonValue == `square`) {
+    num1 = Number(displayContent);
+    operator = buttonValue;
+    doMath(num1, operator);
+    displayContent = result;
+  }
+  else if (buttonValue == `sqrt`) {
+    num1 = Number(displayContent);
+    operator = buttonValue;
+    doMath(num1, operator);
+    displayContent = result;
+    result = parseFloat(result);
   }
   updateDisplay(buttonValue);
   Display();
@@ -88,8 +140,17 @@ function doMath(firstNumber, operator, secondNumber){
         result = `Division by 0 impossible!`;
       }
       break;
+    case `square`:
+      result = Math.pow(firstNumber, 2);
+      break;
+    case `sqrt`:
+      result = Math.sqrt(firstNumber);
+      break;
   }
-  if(isFloat(result)){
+  if (Math.abs(result) > 1e10) {
+    result = result.toExponential(3);
+  }
+  else if(isFloat(result)){
     result = parseFloat(result).toFixed(3);
   }
   displayContent = result.toString();
